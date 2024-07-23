9281b631aa8248f61cc617053922bbb669858f73
zapds <71247896+zapds@users.noreply.github.com>
2024-07-23 13:15:37 +0545

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+
+
+<script>
+
+import "../app.css";
+import { ModeWatcher } from "mode-watcher";
+
+
+</script>
+
+
+<ModeWatcher />
+<slot>
+    
+</slot>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Button } from "$lib/components/ui/button";
+	import { Input } from "$lib/components/ui/input";
+</script>
+
+
+<div class="h-screen flex flex-col items-center mt-24 gap-8">
+	<h1 class="text-8xl">Cricket</h1>
+	<p>Online multiplayer inspired by the original hand-cricket game.</p>
+	<form class="flex flex-col items-center gap-4">
+		<Input class="w-min" autofocus placeholder="Nickname" />
+		<Button type="submit">Play</Button>
+
+	</form>
+</div>
